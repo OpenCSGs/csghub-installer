0c71c6f03736b2b064e0408eb3ed00de73955a0a
mason <xiang.ma@opencsg.com>
2024-09-26 09:53:36 +0800

    Update install.sh

--- helm-chart/install.sh
+++ helm-chart/install.sh
@@ -2,9 +2,9 @@
 
 # Usage
 if [ $# -ne 1 ]; then
-    echo "$0 <domain>"
-    echo "  eg: $0 example.com"
-    exit 1
+  echo "$0 <domain>"
+  echo "  eg: $0 example.com"
+  exit 1
 fi
 
 DOMAIN="$1"
@@ -120,7 +120,7 @@ mirrors:
       "^rancher/(.*)": "opencsg_public/rancher/\$1"
 EOF
 
-log  "INFO"  "Installing K3S..."
+log "INFO" "Installing K3S..."
 retry curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn INSTALL_K3S_VERSION=v1.30.4+k3s1 sh -
 
 # Check if the K3S installation was successful
@@ -164,17 +164,17 @@ if [ $? -ne 0 ]; then
 fi
 
 # Copy the kube config file to the user's home directory
-log  "INFO" "Copying kube config file to the user's home directory."
+log "INFO" "Copying kube config file to the user's home directory."
 mkdir ~/.kube &>/dev/null
 cp -f /etc/rancher/k3s/k3s.yaml ~/.kube/config && chmod 0400 ~/.kube/config
 sed -i "s/127.0.0.1/${IP_ADDRESS}/g" ~/.kube/config
 
 # Install Helm3
-log  "INFO"  "Installing Helm3..."
+log "INFO" "Installing Helm3..."
+retry curl -fsSL -o get_helm.sh https://ghp.ci/https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod +x get_helm.sh && ./get_helm.sh
 if helm version &> /dev/null; then
   log "INFO" "Helm installation verified successfully."
 else
-  retry curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod +x get_helm.sh && ./get_helm.sh
   log "ERRO" "Helm installation verification failed."
   exit 1
 fi
@@ -289,18 +289,18 @@ retry kubectl patch cm config-autoscaler -n knative-serving -p='{"data":{"enable
 
 # Verify if KNative serving resources created successful
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to install Knative Serving crds and core components."
-    exit 1
+  log "ERRO" "Failed to install Knative Serving crds and core components."
+  exit 1
 else
-    log "INFO" "Knative Serving crds and core components installed."
+  log "INFO" "Knative Serving crds and core components installed."
 fi
 
 log "INFO" "Install a networking layer."
 retry kubectl apply -f https://ghp.ci/https://raw.githubusercontent.com/OpenCSGs/CSGHub-Installer/refs/heads/main/helm-chart/knative/kourier.yaml
 # Verify if networking layer installed
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to install kourier networking layer."
-    exit 1
+  log "ERRO" "Failed to install kourier networking layer."
+  exit 1
 fi
 
 log "INFO" "Configure Knative Serving to use Kourier."
@@ -319,8 +319,8 @@ retry kubectl -n kourier-system patch service kourier -p '{"spec": {"type": "Nod
 
 # Verify if Kourier patched with service type NodePort
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to patch Kourier to NodePort."
-    exit 1
+  log "ERRO" "Failed to patch Kourier to NodePort."
+  exit 1
 fi
 
 # Verify that the Knative Serving pod is running
@@ -360,8 +360,8 @@ retry kubectl patch configmap/config-domain \
   --patch '{"data":{"app.internal":""}}'
 # Verify if Kourier patched with service type NodePort
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to patch Knative Serving configure to use self internal domain."
-    exit 1
+  log "ERRO" "Failed to patch Knative Serving configure to use self internal domain."
+  exit 1
 fi
 
 log "INFO" "Creating kube-configs secrets."
@@ -372,15 +372,15 @@ kubectl -n csghub delete secret kube-configs &>/dev/null
 retry kubectl -n csghub create secret generic kube-configs --from-file=/root/.kube/
 
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to create kube configs secrets."
-    exit 1
+  log "ERRO" "Failed to create kube configs secrets."
+  exit 1
 fi
 
 log "INFO" "Add CSGHUB helm repository."
 retry helm repo add csghub https://opencsgs.github.io/CSGHub-Installer --force-update && helm repo update
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to add csghub helm repository."
-    exit 1
+  log "ERRO" "Failed to add csghub helm repository."
+  exit 1
 fi
 
 log "INFO" "Installing CSGHub helm chart..."
@@ -389,8 +389,8 @@ CHART_VERSION=$(helm search repo csghub -l | sort --version-sort -r | awk 'NR==1
 rm -rf csghub-${CHART_VERSION}.tgz &>/dev/null
 retry wget "https://ghp.ci/https://github.com/OpenCSGs/CSGHub-Installer/releases/download/csghub-${CHART_VERSION}/csghub-${CHART_VERSION}.tgz"
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to download csghub helm chart latest version."
-    exit 1
+  log "ERRO" "Failed to download csghub helm chart latest version."
+  exit 1
 fi
 
 # Install helm chart
@@ -404,8 +404,8 @@ retry helm upgrade --install csghub ./csghub-${CHART_VERSION}.tgz \
 
 # Verify if csghub helm chart installed
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to install csghub helm chart."
-    exit 1
+  log "ERRO" "Failed to install csghub helm chart."
+  exit 1
 fi
 
 log "INFO" "Patching ingress service to NodePort."
@@ -415,8 +415,8 @@ log "INFO" "Get the registry self-signed ca certificate."
 retry kubectl -n csghub get secret csghub-certs -ojsonpath='{.data.ca\.crt}' | base64 -d > ca.crt
 # Verify if registry tls secret fetched
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to get registry ca certificate."
-    exit 1
+  log "ERRO" "Failed to get registry ca certificate."
+  exit 1
 fi
 
 log "INFO" "Patching Knative Serving controller."
@@ -457,15 +457,15 @@ retry kubectl -n knative-serving patch deployment.apps/controller --type=json -p
 
 # Verify if Knative Serving controller patched
 if [ $? -ne 0 ]; then
-    log "ERRO" "Failed to patch Knative Serving controller."
-    exit 1
+  log "ERRO" "Failed to patch Knative Serving controller."
+  exit 1
 fi
 
 log "INFO" "Adding insecure registry to k3s."
 SECRET_JSON=$(kubectl -n csghub get secret csghub-registry-docker-config -ojsonpath='{.data.\.dockerconfigjson}' | base64 -d)
 REGISTRY=$(echo "$SECRET_JSON" | jq -r '.auths | keys[]')
 RUSERNAME=$(echo "$SECRET_JSON" | jq -r '.auths | to_entries[] | .value | .username')
-RPASSWORD=$(echo  "$SECRET_JSON" | jq -r '.auths | to_entries[] | .value | .password')
+RPASSWORD=$(echo "$SECRET_JSON" | jq -r '.auths | to_entries[] | .value | .password')
 
 cat <<EOF > /etc/rancher/k3s/registries.yaml
 mirrors:
@@ -490,8 +490,8 @@ log "INFO" "Restarting k3s..."
 systemctl restart k3s
 # Verify if k3s running
 if [[ $(systemctl is-active k3s) != "active" ]]; then
-    log "ERRO" "Failed to install csghub helm chart."
-    exit 1
+  log "ERRO" "Failed to install csghub helm chart."
+  exit 1
 else
   log "INFO" "k3s restarted successfully."
 fi
