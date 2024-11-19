e3e094dbae55ad8a7e4b7de1d32a870a710d146d
mason <xiang.ma@opencsg.com>
2024-11-19 09:55:00 +0800

    Update csghub docker to v1.1.0

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -10,13 +10,14 @@ echo "Creating directories."
 mkdir -p /var/{log,opt}/{postgresql,minio,redis,registry,gitaly,gitlab-shell,nats,casdoor,nginx,space-builder} 2>/dev/null
 mkdir -p /var/run/{postgresql,redis} 2>/dev/null
 mkdir -p /var/nginx/client_body_temp 2>/dev/null
-mkdir -p /var/log/{csghub-server,csghub-accounting,csghub-runner,csghub-user,csghub-rproxy,csghub-portal,mirror-lfs,mirror-repo,dnsmasq} 2>/dev/null
+mkdir -p /var/log/{csghub-server,csghub-accounting,csghub-runner,csghub-user,csghub-rproxy,csghub-portal,csghub-moderation,mirror-lfs,mirror-repo,dnsmasq,temporal} 2>/dev/null
 chown -R postgres:postgres /var/{opt,log,run}/postgresql
 chown -R redis:redis /var/{opt,log,run}/redis
 chown -R registry:registry /var/{opt,log}/registry /etc/registry
 chown -R minio:minio /var/{opt,log}/minio
 chown -R git:git /var/{opt,log}/gitaly /etc/gitaly
 chown -R nats:nats /var/{opt,log}/nats /etc/nats
+chown -R temporal:temporal /etc/temporal
 
 # Create link for fixed space-builder using script directory
 ln -sf /scripts /script
--- docker/scripts/postgres-init-job.sh
+++ docker/scripts/postgres-init-job.sh
@@ -21,12 +21,23 @@ create_user() {
   /usr/bin/psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB} TO ${USER};"
 }
 
+promote_user() {
+  if [ -z "$1" ]; then
+    return 1
+  fi
+
+  USER=$1
+  /usr/bin/psql -c "ALTER USER ${USER} SUPERUSER;"
+}
+
 check_postgresql
 
 echo "Initialize postgres databases"
 create_user "csghub_server"
 create_user "csghub_portal"
 create_user "casdoor"
+create_user "temporal"
+promote_user "temporal"
 
 IF_EXISTS=$(/usr/bin/psql -t -A -l | egrep 'csghub|casdoor' | wc -l)
 if [ "$IF_EXISTS" -eq 6 ]; then
