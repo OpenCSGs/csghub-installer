b7eebd6ac51370754d981eb366b6927d939ee858
wayne liu <43563618+wayneliu0019@users.noreply.github.com>
2024-11-27 10:37:57 +0800

    remove moderation service from compose and create root user trigger first

--- docker-compose/init-scripts/init.sql
+++ docker-compose/init-scripts/init.sql
@@ -14,6 +14,33 @@ SET row_security = off;
 SELECT pg_catalog.set_config('search_path', 'public', false);
 
 --
+-- Seed Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+CREATE OR REPLACE FUNCTION promote_root_to_admin()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.username = 'root' THEN
+        UPDATE public.users
+        SET role_mask = 'admin'
+        WHERE username = 'root';
+
+        -- After update Drop all
+        EXECUTE 'DROP TRIGGER IF EXISTS trigger_promote_root_to_admin ON public.users';
+        EXECUTE 'DROP FUNCTION IF EXISTS promote_root_to_admin()';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql VOLATILE;
+
+CREATE OR REPLACE TRIGGER trigger_promote_root_to_admin
+    AFTER INSERT ON public.users
+    FOR EACH ROW
+EXECUTE FUNCTION promote_root_to_admin();
+
+
+--
 -- Truncate Data for Name: space_resources; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -243,28 +270,3 @@ SELECT pg_catalog.setval(
                true
        );
 
---
--- Seed Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-CREATE OR REPLACE FUNCTION promote_root_to_admin()
-    RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.username = 'root' THEN
-        UPDATE public.users
-        SET role_mask = 'admin'
-        WHERE username = 'root';
-
-        -- After update Drop all
-        EXECUTE 'DROP TRIGGER IF EXISTS trigger_promote_root_to_admin ON public.users';
-        EXECUTE 'DROP FUNCTION IF EXISTS promote_root_to_admin()';
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql VOLATILE;
-
-CREATE OR REPLACE TRIGGER trigger_promote_root_to_admin
-    AFTER INSERT ON public.users
-    FOR EACH ROW
-EXECUTE FUNCTION promote_root_to_admin();
