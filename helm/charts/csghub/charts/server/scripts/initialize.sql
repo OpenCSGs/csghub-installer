9027ab35e75db62195721edab3994d39504cfe57
mason <xiang.ma@opencsg.com>
2025-02-07 11:29:01 +0800

    [Helm] Add new job gpu-gather to auto-label and resource generate for db

--- helm/charts/csghub/charts/server/scripts/initialize.sql
+++ helm/charts/csghub/charts/server/scripts/initialize.sql
@@ -69,14 +69,7 @@ WITH selected_cluster AS (
     INSERT INTO public.space_resources (id, name, resources, cluster_id)
         VALUES
         (1, 'CPU basic · 0.5 vCPU · 1 GB', '{ "cpu": { "type": "Intel", "num": "0.5" }, "memory": "1Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (2, 'CPU basic · 2 vCPU · 4 GB', '{ "cpu": { "type": "Intel", "num": "2" }, "memory": "4Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (3, 'NVIDIA A10G · 4 vCPU · 16 GB', '{"gpu": { "type": "A10", "num": "1", "resource_name": "nvidia.com/gpu", "labels": { "aliyun.accelerator/nvidia_name": "NVIDIA-A10" } }, "cpu": { "type": "Intel", "num": "4" }, "memory": "16Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (4, 'NVIDIA A10G · 6 vCPU · 32 GB', '{"gpu": { "type": "A10", "num": "1", "resource_name": "nvidia.com/gpu", "labels": { "aliyun.accelerator/nvidia_name": "NVIDIA-A10" } }, "cpu": { "type": "Intel", "num": "6" }, "memory": "32Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (5, 'NVIDIA A10G · 2 · 12 vCPU · 48 GB', '{"gpu": { "type": "A10", "num": "2", "resource_name": "nvidia.com/gpu", "labels": { "aliyun.accelerator/nvidia_name": "NVIDIA-A10" } }, "cpu": { "type": "Intel", "num": "12" }, "memory": "48Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (6, 'NVIDIA A10G · 4 · 24 vCPU · 96 GB', '{"gpu": { "type": "A10", "num": "4", "resource_name": "nvidia.com/gpu", "labels": { "aliyun.accelerator/nvidia_name": "NVIDIA-A10" } }, "cpu": { "type": "Intel", "num": "24" }, "memory": "96Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (7, 'NVIDIA A40G · 4 vCPU · 16 GB', '{"gpu": { "type": "A40", "num": "1", "resource_name": "nvidia.com/gpu", "labels": { "nvidia.com/nvidia_name": "NVIDIA-A40" } }, "cpu": { "type": "Intel", "num": "4" }, "memory": "16Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (8, 'NVIDIA A40G · 8 vCPU · 32 GB', '{"gpu": { "type": "A40", "num": "1", "resource_name": "nvidia.com/gpu", "labels": { "nvidia.com/nvidia_name": "NVIDIA-A40" } }, "cpu": { "type": "Intel", "num": "8" }, "memory": "32Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (9, 'NVIDIA A40G · 2 · 12 vCPU · 48 GB', '{"gpu": { "type": "A40", "num": "2", "resource_name": "nvidia.com/gpu", "labels": { "nvidia.com/nvidia_name": "NVIDIA-A40" } }, "cpu": { "type": "Intel", "num": "12" }, "memory": "48Gi" }', ( SELECT cluster_id FROM selected_cluster))
+        (2, 'CPU basic · 2 vCPU · 4 GB', '{ "cpu": { "type": "Intel", "num": "2" }, "memory": "4Gi" }', ( SELECT cluster_id FROM selected_cluster))
 ON CONFLICT (id)
     DO UPDATE SET
         name = EXCLUDED.name,
