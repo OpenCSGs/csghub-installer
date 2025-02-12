4a20c8d5d5b3ac80a03cf5462572f82b627b52a7
mason <xiang.ma@opencsg.com>
2025-02-12 11:42:14 +0800

    [Docker] Refactor minio and postgres init job

--- docker/scripts/minio-init-job.sh
+++ docker/scripts/minio-init-job.sh
@@ -13,13 +13,12 @@ check_minio
 echo "Initialize minio buckets"
 export MC_CONFIG_DIR=/tmp
 mc alias set myMinio "http://127.0.0.1:9000" "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}"
-mc mb myMinio/csghub-server
-mc mb myMinio/csghub-portal
-mc mb myMinio/csghub-registry
-
-IF_EXISTS=$(mc ls myMinio | wc -l)
-if [ "$IF_EXISTS" -eq 3 ]; then
-    exit 0
-else
-    exit 1
-fi
+echo "Create buckets..."
+for BUCKET in "$STARHUB_SERVER_S3_BUCKET" "$CSGHUB_PORTAL_S3_BUCKET" "$REGISTRY_S3_BUCKET"; do
+  if ! mc ls myMinio/"$BUCKET"; then
+    mc mb myMinio/"$BUCKET"
+    echo "Bucket $BUCKET created."
+  else
+    echo "Bucket $BUCKET already exits."
+  fi
+done
