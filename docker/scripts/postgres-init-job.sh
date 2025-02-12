49855c4d751afea767174462d16ea682d7beb430
mason <xiang.ma@opencsg.com>
2025-02-12 11:42:14 +0800

    [Docker] Refactor minio and postgres init job

--- docker/scripts/postgres-init-job.sh
+++ docker/scripts/postgres-init-job.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 check_postgresql() {
   echo "Waiting PostgreSQL ready..."
   until pg_isready; do
@@ -9,40 +11,56 @@ check_postgresql() {
 }
 
 create_user() {
-  if [ -z "$1" ]; then
+  local USER=$1
+  local PASS=$USER
+  local DB=$USER
+
+  if [ -z "$USER" ]; then
+    echo "Error: No username provided."
     return 1
   fi
 
-  USER=$1
-  PASS=$1
-  DB=$1
-  /usr/bin/psql -c "CREATE USER ${USER} WITH ENCRYPTED PASSWORD '${PASS}';"
-  /usr/bin/psql -c "CREATE DATABASE ${DB} OWNER ${USER};"
-  /usr/bin/psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB} TO ${USER};"
+  echo "Checking if user $USER exists..."
+  IF_USER_EXISTS=$(/usr/bin/psql -A -t -c "SELECT count(1) FROM pg_roles WHERE rolname = '$USER'")
+  if [ "$IF_USER_EXISTS" -eq 0 ]; then
+    echo "Creating user $USER..."
+    /usr/bin/psql -c "CREATE USER \"${USER}\" WITH ENCRYPTED PASSWORD '${PASS}';"
+  else
+    echo "User $USER already exists."
+  fi
+
+  echo "Checking if database $DB exists..."
+  IF_DB_EXISTS=$(/usr/bin/psql -A -t -c "SELECT count(1) FROM pg_database WHERE datname = '$DB'")
+  if [ "$IF_DB_EXISTS" -eq 0 ]; then
+    echo "Creating database $DB..."
+    /usr/bin/psql -c "CREATE DATABASE \"${DB}\" ENCODING 'UTF-8' OWNER \"${USER}\";"
+  else
+    echo "Database $DB already exists."
+  fi
+
+  echo "Granting privileges on database $DB to user $USER..."
+  /usr/bin/psql -c "GRANT ALL PRIVILEGES ON DATABASE \"${DB}\" TO \"${USER}\";"
 }
 
 promote_user() {
-  if [ -z "$1" ]; then
+  local USER=$1
+  if [ -z "$USER" ]; then
+    echo "Error: No username provided for promotion."
     return 1
   fi
 
-  USER=$1
-  /usr/bin/psql -c "ALTER USER ${USER} SUPERUSER;"
+  echo "Promoting user $USER to SUPERUSER..."
+  /usr/bin/psql -c "ALTER USER \"${USER}\" SUPERUSER;"
 }
 
 check_postgresql
 
-echo "Initialize postgres databases"
-create_user "csghub_server"
-create_user "csghub_portal"
-create_user "csghub_casdoor"
-create_user "csghub_temporal"
-promote_user "csghub_server"
-promote_user "csghub_temporal"
+echo "Initializing PostgreSQL databases..."
+for USER in "$POSTGRES_SERVER_USER" "$POSTGRES_PORTAL_USER" "$POSTGRES_CASDOOR_USER" "$POSTGRES_TEMPORAL_USER"; do
+  create_user "$USER"
+done
 
-IF_EXISTS=$(/usr/bin/psql -t -A -l | egrep 'csghub|casdoor' | wc -l)
-if [ "$IF_EXISTS" -eq 6 ]; then
-    exit 0
-else
-    exit 1
-fi
+echo "Promote users with SUPERUSER..."
+for USER in "$POSTGRES_SERVER_USER" "$POSTGRES_TEMPORAL_USER"; do
+  promote_user "$USER"
+done
